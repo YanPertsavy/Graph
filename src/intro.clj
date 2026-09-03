71006d1f75e2c814d3775c627de6cfdc242a5ba8
“Yan <yanpretsavy@gmail.com>
2026-09-03 21:32:06 +0300

    task complete

--- src/intro.clj
+++ src/intro.clj
@@ -2,18 +2,18 @@
   (:require [utils :refer [???]]))
 
 (defn square [x]
-  (???))
+  (* x x))
 
 (defn sum-of-squares [x y]
-  (???))
+  (+ (* x x) (* y y)))
 
 (defn factorial [n]
   (loop [product 1, k 1]
     (if (> k n)
-      (???)
-      (recur (???) (???)))))
+      product
+      (recur (* k product) (inc k)))))
 
 (defn gcd [a b]
   (if (= b 0)
-    (???)
-    (recur (???) (???))))
+    a
+    (recur b (rem a b))))
